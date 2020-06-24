4930b6317db4d2d5b211e5c9bdb46963e84c30fb
UR5NFR <naluster@gmail.com>
2020-06-24 11:00:00 +0300

    Update config_ini.py

--- config_ini.py
+++ config_ini.py
@@ -1,7 +1,6 @@
 # -------------------------------------------------------------------
 # Модуль для работы с ini-файлом
 # 25.01.18
-# Python 3 required
 # -------------------------------------------------------------------
 import configparser, os
 
@@ -22,9 +21,9 @@ def create_config(path):
 	config.set("Settings", "host", "10.0.1.50")
 	config.set("Settings", "port", "25")
 	config.set("Settings", "pause", "20")
-	config.set("Templates", "message", "message.txt")
-	config.set("Templates", "message_ret", "message_ret.txt")
-	config.set("Templates", "message_empty", "message_empty.txt")
+	config.set("Templates", "message", "message.html")
+	config.set("Templates", "message_ret", "message_ret.html")
+	config.set("Templates", "message_empty", "message_empty.html")
 		
 	with open(path, "w") as config_file:
 		config.write(config_file)
@@ -40,6 +39,20 @@ def get_config(path):
 	config.read(path)
 	return config
 	
+def get_all_settings(path):
+	"""
+	Прочитать весь ini подряд
+	"""
+	config = get_config(path)
+	a = {}
+	for each_section in config.sections():
+		for each_key, each_val in config.items(each_section):
+			a.update({each_key: each_val})
+			#return each_key
+			#return each_val
+	for key in a.keys():
+		print(key)
+
 def get_setting(path, section, setting):
 	"""
 	Прочитать установки из конфига
@@ -60,8 +73,7 @@ def update_setting(path, section, setting, value):
 	config.set(section, setting, value)
 	with open(path, "w") as config_file:
 		config.write(config_file)
- 
- 
+
 def delete_setting(path, section, setting):
 	"""
 	Удалить установки
@@ -74,6 +86,7 @@ def delete_setting(path, section, setting):
 # Примеры вызова функций
 #host = get_setting(path, 'Settings', 'host')
 #age = get_setting(path, 'Settings', 'age')
-	
+#get_all_settings(path)
+#print(get_all_settings(path))
 #update_setting(path, "Settings", "age", "12")
 #delete_setting(path, "Settings", "pause")
